b08fc69b59199ad741b80b65b35dc258791d93e7
catdoctor <hellohellomike@gmail.com>
2018-09-05 00:56:06 +0100

    fix Offsets stuff visibility

--- FF1Lib/Offsets.cs
+++ FF1Lib/Offsets.cs
@@ -2,13 +2,13 @@ namespace FF1Lib
 {
 	public partial class FF1Rom
 	{
-		private static int BA(int bank, int addr)
+		private static class Offsets
 		{
-			return bank*0x4000 + (addr - (bank == 0x1F ? 0xC000 : 0x8000));
-		}
+			private static int BA(int bank, int addr)
+			{
+				return bank*0x4000 + (addr - (bank == 0x1F ? 0xC000 : 0x8000));
+			}
 
-		internal static class Offsets
-		{
 			public static int lut_PtyGenBuf = BA(0x1E, 0x84AA);
 			public static int lut_AllowedClasses = BA(0x1E, 0x8128);
 		}
