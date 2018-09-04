136551d125c44e93f8916986a210a583c4ccc826
catdoctor <hellohellomike@gmail.com>
2018-09-05 00:54:02 +0100

    Add an Offsets internal class for stuff

---
+++ FF1Lib/Offsets.cs
@@ -0,0 +1,16 @@
+namespace FF1Lib
+{
+	public partial class FF1Rom
+	{
+		private static int BA(int bank, int addr)
+		{
+			return bank*0x4000 + (addr - (bank == 0x1F ? 0xC000 : 0x8000));
+		}
+
+		internal static class Offsets
+		{
+			public static int lut_PtyGenBuf = BA(0x1E, 0x84AA);
+			public static int lut_AllowedClasses = BA(0x1E, 0x8128);
+		}
+	}
+}
